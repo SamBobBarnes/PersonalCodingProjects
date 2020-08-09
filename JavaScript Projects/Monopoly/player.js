80ccdfcb1dbf0b1a79336e240e4e13dbdc7b9fae
SamBobBarnes <54638834+SamBobBarnes@users.noreply.github.com>
2020-08-08 20:04:20 -0500

    formed out much of the basic function of the player

--- JavaScript Projects/Monopoly/player.js
+++ JavaScript Projects/Monopoly/player.js
@@ -3,10 +3,47 @@
 class Player{
   constructor(name){
     this.name = name;
+    this.money = 1500;
+    this.properties = [];
   }
 
   getName() {
     return this.name;
   }
+
+  balance(){
+    return this.money;
+  }
+
+  addMoney(sum){
+    this.money += sum;
+  }
+
+  payMoney(sum){
+    this.money -= sum;
+  }
+
+  getProperties(){
+    return this.properties;
+  }
+
+  getNumProperties(){
+    return this.properties.length;
+  }
+
+  addProperty(property){
+    this.properties.push(property);
+  }
+
+  removeProperty(prop){
+    var newProps = [];
+    this.properties.forEach((item, i) => {
+      if(item.name != prop){
+        newProps.push(item);
+      }
+    });
+    this.properties = newProps;
+  }
+
 
 }
