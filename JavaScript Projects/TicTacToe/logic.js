49cc0de6b453e4df1ff3607ac2e40af9216747ac
SamBobBarnes <samuelrbarnes@outlook.com>
2020-08-16 21:40:18 -0500

    started minimax algorithm

--- JavaScript Projects/TicTacToe/logic.js
+++ JavaScript Projects/TicTacToe/logic.js
@@ -1,1 +1,15 @@
 /* logic.js */
+
+
+function minimax(position, depth, isMaximizing){
+  score = 0;
+  let bestScore = -Infinity;
+  if(depth == 0) return score;
+
+  if(isMaximizing){
+    let bestScore = -Infinity;
+    
+  } else {
+    let bestScore = Infinity;
+  }
+}
