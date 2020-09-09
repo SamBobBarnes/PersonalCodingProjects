ccdbcaa99edddbbb747bd4bff89ced24d9db79d1
SamBobBarnes <samuelrbarnes@outlook.com>
2020-09-09 12:55:58 -0500

    working on react tutorial first commit

--- ReactJS/hello-world/src/App.js
+++ ReactJS/hello-world/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Greet } from './Components/Greet'
+import Welcome from './Components/Welcome'
+import Hello from './Components/Hello'
+import Message from './Components/Message'
+import Counter from './Components/Counter'
+import FunctionClick from './Components/FunctionClick'
+import ClassClick from './Components/ClassClick'
+import EventBind from './Components/EventBind'
+import ParentComponent from './Components/ParentComponent'
+import UserGreeting from './Components/UserGreeting';
+import NameList from './Components/NameList';
+import Stylesheet from './Components/Stylesheet'
+import Inline from './Components/Inline';
+import './AppStyles.css'
+import styles from './AppStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1> */}
+      {/* <Inline /> */}
+      {/* <Stylesheet primary={true}/> */}
+      {/* <NameList /> */}
+      {/* <UserGreeting /> */}
+      {/* <ParentComponent /> */}
+      {/* <EventBind /> */}
+      {/* <ClassClick /> */}
+      {/* <FunctionClick /> */}
+      {/* <Counter /> */}
+      {/* <Message /> */}
+      {/* <Greet name='Bruce' heroName='Batman'>
+        <p>This is children props</p>
+      </Greet>
+      <Greet name='Clark' heroName='Superman'>
+        <button>Action</button>
+      </Greet>
+      <Greet name='Diana' heroName='Wonder Woman' /> */}
+      {/* <Welcome name='Bruce' heroName='Batman' /> */}
+      {/* <Welcome name='Clark' heroName='Superman' /> */}
+      {/* <Welcome name='Diana' heroName='Wonder Woman' /> */}
+      {/* <Hello /> */}
     </div>
   );
 }
