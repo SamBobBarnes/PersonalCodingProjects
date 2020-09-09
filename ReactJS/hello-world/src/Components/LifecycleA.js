f6dc08585f3418f4097c21186c3b54d03b601007
SamBobBarnes <54638834+SamBobBarnes@users.noreply.github.com>
2020-09-09 14:22:47 -0500

    React Video 24

--- ReactJS/hello-world/src/Components/LifecycleA.js
+++ ReactJS/hello-world/src/Components/LifecycleA.js
@@ -20,11 +20,32 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount')
     }
 
+    shouldComponentUpdate() {
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'Barnes'
+        })
+    }
+
     render() {
         console.log('LifecycleA render')
         return (
         <div>
             <div>Lifecycle A</div>
+            <button onClick={this.changeState}>Change State</button>
             <LifecycleB />
         </div>
         
