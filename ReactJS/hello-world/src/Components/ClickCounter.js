65fab925f33e7c1a5c5253d3a1a3cab8921f1ce8
SamBobBarnes <54638834+SamBobBarnes@users.noreply.github.com>
2020-09-09 20:03:08 -0500

    React Video 35

--- ReactJS/hello-world/src/Components/ClickCounter.js
+++ ReactJS/hello-world/src/Components/ClickCounter.js
@@ -6,10 +6,10 @@ class ClickCounter extends Component {
         const { count, incrementCount } = this.props
         return (
             <button onClick={incrementCount}>
-                Clicked {count} times
+                {this.props.name} Clicked {count} times
             </button>
         )
     }
 }
 
-export default withCounter(ClickCounter)
+export default withCounter(ClickCounter, 5)
